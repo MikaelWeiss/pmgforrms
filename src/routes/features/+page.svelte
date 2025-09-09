cb429a8a1d3a3af45f186de5c069916f1baef9ed
Mikael Weiss <campingmikael@icloud.com>
2025-09-09 12:09:15 -0600

    features page update

--- src/routes/features/+page.svelte
+++ src/routes/features/+page.svelte
@@ -59,7 +59,6 @@
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
 					</svg>
 					<p>Daily Intentions Interface</p>
-					<p class="text-sm">App screenshot placeholder</p>
 				</div>
 			</div>
 		</div>
@@ -72,7 +71,6 @@
 							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
 						</svg>
 						<p>Goal Tracking Interface</p>
-						<p class="text-sm">App screenshot placeholder</p>
 					</div>
 				</div>
 			</div>
@@ -149,7 +147,6 @@
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
 					</svg>
 					<p>Calendar Interface</p>
-					<p class="text-sm">App screenshot placeholder</p>
 				</div>
 			</div>
 		</div>
@@ -162,7 +159,6 @@
 							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
 						</svg>
 						<p>People Management Interface</p>
-						<p class="text-sm">App screenshot placeholder</p>
 					</div>
 				</div>
 			</div>
@@ -239,7 +235,6 @@
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10"></path>
 					</svg>
 					<p>Cloud Sync Interface</p>
-					<p class="text-sm">App screenshot placeholder</p>
 				</div>
 			</div>
 		</div>
